fb66d393890324fd82b01fd6d52883a5585ee489
Sean <Sean.Donahue.5311@gmail.com>
2020-10-01 10:46:04 -0400

    Fixes to widgets on forms

--- query_app/forms.py
+++ query_app/forms.py
@@ -3,11 +3,11 @@ from django.contrib.postgres.forms import SimpleArrayField
 
 
 class QueryForm(forms.Form):
-    output_type = forms.ChoiceField(choices=(('Gene', None), ('Cell', None), ('Organ', None)), widget=forms.Select)
+    output_type = forms.ChoiceField(choices=(('Gene', 'Gene'), ('Cell', 'Cell'), ('Organ', 'Organ')), widget=forms.Select)
 
 
 class GeneQueryForm(forms.Form):
-    input_type = forms.ChoiceField(choices=(('Organ', None),), required=True, widget=forms.Select)
+    input_type = forms.ChoiceField(choices=(('Organ', 'Organ'),), required=True, widget=forms.Select)
     input_set = forms.CharField(max_length=1024, required=True, widget=forms.Textarea)
     logical_operator = forms.ChoiceField(choices=(('and','and'),('or', 'or')), required=True, widget=forms.Select)
     marker = forms.ChoiceField(choices=(('True', 'True'), ('False', 'False')), required=False, widget=forms.Select)
@@ -15,7 +15,7 @@ class GeneQueryForm(forms.Form):
 
 
 class OrganQueryForm(forms.Form):
-    input_type = forms.ChoiceField(choices=(('Cell', None), ('Gene', None)), required=True, widget=forms.Select)
+    input_type = forms.ChoiceField(choices=(('Cell', 'Cell'), ('Gene', 'Gene')), required=True, widget=forms.Select)
     input_set = forms.CharField(max_length=1024, required=True, widget=forms.Textarea)
     logical_operator = forms.ChoiceField(choices=(('and', 'and'), ('or', 'or')), required=True, widget=forms.Select)
     marker = forms.ChoiceField(choices=(('True', 'True'), ('False', 'False')), widget=forms.Select)
@@ -23,7 +23,7 @@ class OrganQueryForm(forms.Form):
 
 
 class CellQueryForm(forms.Form):
-    input_type = forms.ChoiceField(choices=(('Gene', None), ('Protein', None), ('Organ', None)), required=True, widget=forms.Select)
+    input_type = forms.ChoiceField(choices=(('Gene', 'Gene'), ('Protein', 'Protein'), ('Organ', 'Organ')), required=True, widget=forms.Select)
     input_set = forms.CharField(max_length=1024, required=True, widget=forms.Textarea)
     logical_operator = forms.ChoiceField(choices=(('and', 'and'), ('or', 'or')), required=True, widget=forms.Select)
     genomic_modality = forms.ChoiceField(choices=(('rna', 'rna'), ('atac', 'atac')), required=False, widget=forms.Select)
