083f73f78f4100bde8fcf5a19629c9c62323524e
Sean <Sean.Donahue.5311@gmail.com>
2022-04-12 12:49:29 -0400

    Changing lists to set literals for membership checking

--- hubmap/query_app/queries.py
+++ hubmap/query_app/queries.py
@@ -101,15 +101,7 @@ def get_datasets_list(query_params: Dict, input_set=None):
     query_params = process_query_parameters(query_params, input_set)
     filter = get_dataset_filter(query_params)
 
-    if query_params["input_type"] in [
-        "cell",
-        "cell_type",
-        "cluster",
-        "dataset",
-        "gene",
-        "modality",
-        "protein",
-    ]:
+    if query_params["input_type"] in {"cell", "cluster", "dataset", "gene", "modality", "protein"}:
         query_set = (
             Dataset.objects.filter(filter)
             .filter(modality__modality_name__isnull=False)
@@ -123,7 +115,7 @@ def get_cell_types_list(query_params: Dict, input_set=None):
     query_params = process_query_parameters(query_params, input_set)
     filter = get_cell_type_filter(query_params)
 
-    if query_params["input_type"] in ["cell", "cell_type", "dataset"]:
+    if query_params["input_type"] in {"cell", "cell_type", "dataset"}:
         query_set = CellType.objects.filter(filter)
         query_handle = make_pickle_and_hash(query_set, "cell_type")
         return query_handle
