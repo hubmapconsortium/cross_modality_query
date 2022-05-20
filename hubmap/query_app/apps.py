47359f12f3e17908287221fe3094a02cc719b21f
Sean <Sean.Donahue.5311@gmail.com>
2022-05-19 22:28:33 -0400

    Fixing oops from commit revert merge

--- hubmap/query_app/apps.py
+++ hubmap/query_app/apps.py
@@ -125,13 +125,8 @@ def attempt_to_open_file(file_path, key=None):
                     if isinstance(df["clusters"].iloc[0], str):
                         df["clusters"] = df["clusters"].str.split(",")
 
-                    old_clusters_list = df["clusters"].to_list()
-                    cell_types_list = df["cell_type"].to_list()
-                    new_clusters_list = [
-                        old_clusters_list[i] + [cell_types_list[i]]
-                        for i in range(len(old_clusters_list))
-                    ]
-                    df["clusters"] = pd.Series(new_clusters_list, index=df.index)
+                if "cell_type" not in df.columns:
+                    df["cell_type"] = "unknown"
 
             df = df.set_index(columns_dict[key], drop=False, inplace=False).sort_index()
 
